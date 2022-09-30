f92f6faf0fdedbe20a16f54377fe09f3c7c35121
maria2332 <113980788+maria2332@users.noreply.github.com>
2022-09-30 11:25:02 +0000

    ejercicio2

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-
+#ejercicio 2
+numero1=int(input('Introduce un número: '))
+print(str(numero1).zfill(6))
+print("\n")
+numero2=float(input('Introduce otro número: '))
+print("{:09.03f}".format(numero2))
